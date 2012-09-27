06ef36e793bc334f7884ed8f89dba3d9f0557714
Sergej <lukashov_sergej@live.com>
2012-09-27 22:49:56 +0300

    done :)

---
+++ Modify_project_files.pro
@@ -0,0 +1,18 @@
+#-------------------------------------------------
+#
+# Project created by QtCreator 2012-09-27T17:01:56
+#
+#-------------------------------------------------
+
+QT       += core
+
+QT       -= gui
+
+TARGET = Modify_project_files
+CONFIG   += console
+CONFIG   -= app_bundle
+
+TEMPLATE = app
+
+
+SOURCES += main.cpp
---
+++ main.cpp
@@ -0,0 +1,55 @@
+#include <QtCore/QCoreApplication>
+//#include <stdio.h>
+#include <iostream>
+#include <QFile>
+#include <QTextStream>
+#include <QRegExp>
+
+void updateFile(char * fileName);
+
+int main(int argc, char *argv[])
+{
+    QCoreApplication a(argc, argv);
+
+    char *fileName=new char[100];
+    //char* fileName="d:\\Programming\\Qt prog's\\Modify_project_files\\debug\\graphics.html";
+    std::cout<<"Enter file name to update: ";
+    std::cin>>fileName;
+    updateFile(fileName);
+
+    delete[] fileName;
+
+    return a.exec();
+}
+
+void updateFile(char * fileName)
+{
+    QFile inputFile(fileName);
+    if(!inputFile.open(QFile::ReadWrite | QFile::Text))
+    {
+        std::cout<<"Opening file error"<<std::endl;
+        return;
+    }
+
+    QTextStream ts(&inputFile);
+    QString temp=ts.readAll();
+    if(temp.contains("</h1>"))
+    {
+        temp.insert(temp.indexOf("</h1>")+5,"\n\t\t<hr>");
+    }
+    inputFile.close();
+
+    QFile outputFile(fileName);
+    if(!outputFile.open(QFile::WriteOnly | QFile::Text))
+    {
+        std::cout<<"Opening file error"<<std::endl;
+        return;
+    }
+
+    QTextStream textStream(&outputFile);
+    textStream<<temp;
+    textStream.flush();
+    outputFile.close();
+
+    std::cout<<"File updated"<<std::endl;
+}
